1703d61b616b9d6e255f8ba038b9a6417d5accc2
Mitchxxx <megboko@gmail.com>
2024-06-03 23:35:04 +0100

    Update S3.tf to create bucket_out

--- Terraform/s3.tf
+++ Terraform/s3.tf
@@ -13,3 +13,21 @@ resource "aws_s3_bucket_cors_configuration" "bucket_in_cors" {
     allowed_origins = ["*"]
   }
 }
+
+# S3 bucket for processed files ("bucket-out")
+resource "aws_s3_bucket" "bucket_out" {
+  bucket = "${var.s3_bucket_prefix}-out"
+}
+
+
+resource "aws_s3_bucket_cors_configuration" "bucket_out_cors" {
+  bucket = aws_s3_bucket.bucket_out.id
+
+  cors_rule {
+    allowed_headers = ["*"]
+    allowed_methods = ["PUT", "POST"]
+    allowed_origins = ["*"]
+    expose_headers  = ["ETag"]
+    max_age_seconds = 3000
+  }
+}
